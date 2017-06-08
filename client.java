b7f8292beaa5f411b106f37057e03cc0ec1fbe99
kesava5253 <ratnakesavareddy@gmail.com>
2017-06-08 11:11:12 +0530

    added printout stat

--- client.java
+++ client.java
@@ -3,9 +3,10 @@ import java.net.*;
 public class client {  
 public static void main(String[] args) {  
 try{      
-Socket s=new Socket("192.168.0.106",2222);  
+Socket s=new Socket("192.168.0.108",4444);  
 DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
 dout.writeUTF("hi bud its tym to party");  
+dout.writeUTF("this is for git");  
 dout.flush();  
 dout.close();  
 s.close();  

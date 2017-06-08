fdb7672d6aaaa4cae082632a4090cb090abdbdfa
Rihu <nrajashekarreddy94@gmail.com>
2017-06-08 11:20:28 +0530

    Rajashekar Reddy

--- Server.java
+++ Server.java
@@ -1,15 +1,14 @@
     import java.net.*;  
     import java.io.*;  
-    class Server{  
+    class Server
+    {  
     public static void main(String args[])throws Exception{  
     ServerSocket ss=new ServerSocket(3333);  
     Socket s=ss.accept();  
     DataInputStream din=new DataInputStream(s.getInputStream());  
     DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-      
     String str="",str2="";
-  
     while(!str.equals("stop")){  
     str=din.readUTF();  
     System.out.println("client says: "+str);  
@@ -20,4 +19,5 @@
     din.close();  
     s.close();  
     ss.close();  
-    }}  
+    }
+}  
